d514ff646369ba10febef5f6e07b1adfa5272e1e
FGPetz <franciscogpetz@gmail.com>
2021-06-27 18:10:37 -0300

    merge de los 2 mains, ahora debería leer bien la entrada

--- src/Main.kt
+++ src/Main.kt
@@ -1,30 +1,30 @@
 import java.util.*
 
-private const val delimiter = " "
+private const val DELIMITER = " "
 
     fun main(args: Array<String>) {
 
-        var n: Int // cantidad de nodos (1..n)
-        var m: Int // cantidad de arcos
-        var s: Int // nodo source
-        var t: Int // nodo sink
+        val cantidadNodos: Int
+        val cantidadArcos: Int
+        val nodoSource: Int
+        val nodoSink: Int
         var line : String
 
         val sc = Scanner(System.`in`)
-        println("Input: ")
         line = sc.nextLine()
-        n = line.split(delimiter)[0].toInt()
-        m = line.split(delimiter)[1].toInt()
+        cantidadNodos = line.split(DELIMITER)[0].toInt()
+        cantidadArcos = line.split(DELIMITER)[1].toInt()
         line = sc.nextLine()
-        s = line.split(delimiter)[0].toInt()
-        t = line.split(delimiter)[1].toInt()
-
-
-
+        nodoSource = line.split(DELIMITER)[0].toInt()
+        nodoSink = line.split(DELIMITER)[1].toInt()
+        val edgeList: MutableList<String> = mutableListOf()
+        for(i in 1..cantidadArcos){
+            sc.nextLine()?.let { edgeList.add(it) }
+        }
     }
 
     private fun getList(str: String): List<String> {
-        val list = str.split(delimiter)
+        val list = str.split(DELIMITER)
         return list
     }
 
@@ -33,23 +33,4 @@ private const val delimiter = " "
         
     }
 
-fun main2 (args: Array<String>){
-    val quantities = readLine()?.split(" ")
-    val sourceAndSinkNodes = readLine()?.split(" ")
-    var nodes = 0
-    var edges = 0
-    if(quantities != null){
-        nodes = quantities[0].toInt()
-        edges = quantities[1].toInt()
-    }
-    val edgeList: MutableList<String> = mutableListOf()
-    for(i in 1..edges){
-        readLine()?.let { edgeList.add(it) }
-    }
-    println(sourceAndSinkNodes?.let { maxFlux(edgeList, it) })
-}
-
-fun maxFlux(input: List<String>,sourceAndSinkNodes: List<String>): Int {
-    return 0
-}
 
